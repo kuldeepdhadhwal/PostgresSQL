92e9d666c0429597db1de9fb5e904116f6a0542c
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-09-27 18:45:32 +0530

    Create leftouterjoin.sql

---
+++ joins/leftouterjoin.sql
@@ -0,0 +1,4 @@
+Select m1.firstname as memfname, m1.surname as memsname, m2.firstname as recfname, m2.surname as recsname from cd.members m1
+	left outer join cd.members m2
+	on m2.memid = m1.recommendedby
+	order by memsname,memfname;

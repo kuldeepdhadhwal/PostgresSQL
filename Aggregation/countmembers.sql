9d04f8cb3caab67e3f3e85a92a7e3ba1d61d97fd
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-10-26 14:57:21 +0530

    Update countmembers.sql

--- Aggregation/countmembers.sql
+++ Aggregation/countmembers.sql
@@ -1,1 +1,6 @@
 Select count(*) over() as count, firstname, surname from cd.members;
+
+-- Alternate way
+with total_members as (Select count(*) as total_count from cd.members)
+ 
+select total_count as "count", firstname, surname from cd.members cross join total_members;

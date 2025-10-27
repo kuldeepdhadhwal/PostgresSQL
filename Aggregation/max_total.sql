46435c63ec2c49be1fd278d6bcb7ceb1194dffab
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-10-27 14:02:11 +0530

    Update max_total.sql

--- Aggregation/max_total.sql
+++ Aggregation/max_total.sql
@@ -1,3 +1,8 @@
 with booking as (Select facid, sum(slots) as "total" from cd.bookings group by facid)
 
 select facid, total from booking where total = (select max(total) as "total" from booking);
+
+_________________________________________________________________________________________________
+
+Select facid, total from (Select facid, total, rank() over(order by total desc) as rank from (
+  select facid, sum(slots) as "total" from cd.bookings group by facid)) where rank = 1;

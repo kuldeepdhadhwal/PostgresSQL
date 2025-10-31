bb372714099d974e4800de3b848224ef9028fe8c
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-10-31 19:00:10 +0530

    Update timestamp.sql

--- datetime/timestamp.sql
+++ datetime/timestamp.sql
@@ -1,3 +1,5 @@
 Select make_timestamp(2012,08,31,01,00,00) as timestamp;
 
 Select (make_timestamp(2012,08,31,01,00,00) - make_timestamp(2012,07,30,01,00,00)) as interval ; 
+
+select generate_series(timestamp '2012-10-01', timestamp '2012-10-31', interval '1 day') as ts;     

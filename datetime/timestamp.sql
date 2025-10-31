5d5fd66b90731598e52020d4ff114ac16a60a95f
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-10-31 21:13:47 +0530

    Update timestamp.sql

--- datetime/timestamp.sql
+++ datetime/timestamp.sql
@@ -3,3 +3,7 @@ Select make_timestamp(2012,08,31,01,00,00) as timestamp;
 Select (make_timestamp(2012,08,31,01,00,00) - make_timestamp(2012,07,30,01,00,00)) as interval ; 
 
 select generate_series(timestamp '2012-10-01', timestamp '2012-10-31', interval '1 day') as ts;     
+
+-- extract the days
+Select distinct Extract('day' from starttime) as date_part from cd.bookings where starttime > '2012-08-31 00:00:00' and 
+starttime <= '2012-08-31 12:00:00'

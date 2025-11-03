0076d4942ff46c191bd69e63141c936863f43341
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-11-03 15:38:43 +0530

    Update risingtemp.sql

--- LeetcodeSql/risingtemp.sql
+++ LeetcodeSql/risingtemp.sql
@@ -6,3 +6,12 @@ with WeatherTable as  (Select id, temperature, lag(temperature) over( order by i
  from Weather)
 
  Select id from WeatherTable where id !=1 and weather = 'less';
+
+
+
+-- # Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+ AND w1.temperature > w2.temperature;

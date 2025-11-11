61ef804d22c8d3a3ef6efe4d807aa9b2267008e1
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-11-11 12:16:11 +0530

    Create listOfProductsOrdered.sql

---
+++ LeetcodeSql/listOfProductsOrdered.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+Select p.product_name, sum(o.unit) as unit from Products p inner join Orders o on p.product_id = o.product_id  where o.order_date between '2020-02-01' and '2020-02-29' group by p.product_id having sum(o.unit) >= 100;

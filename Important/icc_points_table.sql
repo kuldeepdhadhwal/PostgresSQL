803289d5b6b210b3f203539d6dd09edb0088b15f
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-12-23 18:28:15 +0530

    Add SQL query for ICC points table calculation

---
+++ Important/icc_points_table.sql
@@ -0,0 +1,25 @@
+with my_table as (
+Select Team_1, total_matches, 
+  case 
+    when win_count is Null 
+    then 0 
+    else win_count
+    End as win_count from (
+Select t1.Team_1, t1.total_matches, t2.win_count from (
+SELECT e.Team_1, COUNT(*) as total_matches FROM (SELECT Team_1 from icc_world_cup
+
+UNION all
+
+SELECT Team_2 from icc_world_cup ) e
+GROUP BY Team_1)  t1 
+ left join (
+
+SELECT Winner, Count(Winner) as win_count FROM  icc_world_cup group by Winner)  t2 
+on t1.Team_1 = t2.Winner) t
+)
+
+Select Team_1 as team_name, total_matches as no_of_matches_played,
+    win_count as no_of_matches_won,
+    (total_matches - win_count) as no_of_losses from my_table
+order by no_of_matches_won desc,team_name desc, no_of_losses asc
+;

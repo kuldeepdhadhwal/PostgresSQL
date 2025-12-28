dccda8487df217170fe0015884e66957b5920358
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2025-12-28 16:01:02 +0530

    Enhance unpivot_table.sql with comments and formatting
    Added comments and structured output for clarity.

---
+++ Important/unpivot_table.sql
@@ -0,0 +1,74 @@
+create table emp_compensation (
+emp_id int,
+salary_component_type varchar(20),
+val int
+);
+
+insert into emp_compensation
+values (1,'salary',10000),(1,'bonus',5000),(1,'hike_percent',10)
+, (2,'salary',15000),(2,'bonus',7000),(2,'hike_percent',8)
+, (3,'salary',12000),(3,'bonus',6000),(3,'hike_percent',7);
+
+select * from emp_compensation;
+
+Create Table test_table (
+Select emp_id, 
+  sum(Case when salary_component_type = 'salary' then val End) as Salary,
+  sum(Case when salary_component_type = 'bonus' then val End) as Bonus,
+  sum(Case when salary_component_type = 'hike_percent' then val End) as hike_percent
+
+from emp_compensation
+Group by emp_id );
+
+Select * from test_table;
+
+Select emp_id, 'salary' as salary_component_type, Salary as val from test_table
+
+union all
+
+Select emp_id, 'bonus' as salary_component_type, Bonus as val from test_table
+union all
+
+Select emp_id, 'hike_percent' as salary_component_type, hike_percent as val from test_table
+order by emp_id;
+
+
+
+-- Create Table
++--------+-----------------------+-------+
+| emp_id | salary_component_type | val   |
++--------+-----------------------+-------+
+|      1 | salary                | 10000 |
+|      1 | bonus                 |  5000 |
+|      1 | hike_percent          |    10 |
+|      2 | salary                | 15000 |
+|      2 | bonus                 |  7000 |
+|      2 | hike_percent          |     8 |
+|      3 | salary                | 12000 |
+|      3 | bonus                 |  6000 |
+|      3 | hike_percent          |     7 |
++--------+-----------------------+-------+
+  -- Pivot Table
++--------+--------+-------+--------------+
+| emp_id | Salary | Bonus | hike_percent |
++--------+--------+-------+--------------+
+|      1 |  10000 |  5000 |           10 |
+|      2 |  15000 |  7000 |            8 |
+|      3 |  12000 |  6000 |            7 |
++--------+--------+-------+--------------+
+
+  -- Unpivoted Table
++--------+-----------------------+-------+
+| emp_id | salary_component_type | val   |
++--------+-----------------------+-------+
+|      1 | salary                | 10000 |
+|      1 | bonus                 |  5000 |
+|      1 | hike_percent          |    10 |
+|      2 | salary                | 15000 |
+|      2 | bonus                 |  7000 |
+|      2 | hike_percent          |     8 |
+|      3 | salary                | 12000 |
+|      3 | bonus                 |  6000 |
+|      3 | hike_percent          |     7 |
++--------+-----------------------+-------+
+
